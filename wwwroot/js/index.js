8576c69ec229010e2c33a56d830e09d23ebb83da
Marta Martin Delgado <marta_martin93@hotmail.com>
2022-01-11 06:24:11 +0000

    testing jQuery

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,4 +1,20 @@
 ﻿console.log("hello");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+// in js
+//var theForm = document.getElementById("theForm");
+//theForm.hidden = true;
+
+// with jQuery
+var theForm = $("#theForm");
+theForm.hide();
+// jQuery is more adaptable to the different browsers
+
+var button = $("#updateContent");
+button.on("click", function () {
+    alert("Updating Info");
+});
+
+var musicianInfo = $(".musician-props li");
+musicianInfo.on("click", function () {
+    console.log("You clicked on " + $(this).text());
+});
