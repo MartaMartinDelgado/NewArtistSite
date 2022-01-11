f6ea104077d175b0f5e3e2f2d04b14681a555ac6
Marta Martin Delgado <marta_martin93@hotmail.com>
2022-01-11 07:19:47 +0000

    Added login toggle

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,20 +1,28 @@
-﻿console.log("hello");
+﻿// Usin jQuery as it is more adaptable to the different browsers
 
-// in js
-//var theForm = document.getElementById("theForm");
-//theForm.hidden = true;
+// To execute the block of code only once the page has been completely loaded
+$(document).ready(function () {
+    // All code inside this function is visible to each other
+    // But it is not leaked into the global scope
 
-// with jQuery
-var theForm = $("#theForm");
-theForm.hide();
-// jQuery is more adaptable to the different browsers
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = $("#updateContent");
-button.on("click", function () {
-    console.log("Updating Info");
-});
+    var button = $("#updateContent");
+    button.on("click", function () {
+        console.log("Updating Info");
+    });
 
-var musicianInfo = $(".musician-props li");
-musicianInfo.on("click", function () {
-    console.log("You clicked on " + $(this).text());
+    var musicianInfo = $(".musician-props li");
+    musicianInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    })
+
 });
