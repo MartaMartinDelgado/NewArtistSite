9eda0f62648e74229930b0ce48eb9777bce676c9
Marta Martin Delgado <marta_martin93@hotmail.com>
2022-07-27 21:06:16 +0100

    Login implemented Outstanding Issue: after login, the site does not redirect to Experience page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using ArtistSite.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtistSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<Artist> _signInManager;
+
+        public AccountController(ILogger<AccountController> logger,
+            SignInManager<Artist> signInManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Username,
+                    model.Password,
+                    model.RememberMe,
+                    false);
+
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        RedirectToAction("Experience", "App");
+                    } 
+                }
+            }
+
+            ModelState.AddModelError("", "Failed to Login");
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "App");
+        }
+    }
+}
